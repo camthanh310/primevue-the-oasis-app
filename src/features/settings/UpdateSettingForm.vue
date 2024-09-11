711ab126101dd7478fa52ede810f27004ae36c97
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2024-09-11 20:45:39 +0700

    immediate update setting field

--- src/features/settings/UpdateSettingForm.vue
+++ src/features/settings/UpdateSettingForm.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { reactive, watchEffect } from 'vue'
 import type { Setting } from '@/types/Collection'
 import { useSettings } from '@/composables/settings/useSettings'
 import { useUpdateSetting } from '@/composables/settings/useUpdateSetting'
@@ -7,20 +8,42 @@ import FormRow from '@/components/FormRow.vue'
 import AppInput from '@/components/AppInput.vue'
 import AppSpinner from '@/components/AppSpinner.vue'
 
+type SettingType = Record<Setting, number | string>
+
 const { settings, isLoading } = useSettings()
 const { updateSetting, isUpdating } = useUpdateSetting()
 
+const defaultSetting = reactive<SettingType>({
+  minBookingLength: 0,
+  maxBookingLength: 0,
+  maxGuestsPerBooking: 0,
+  breakfastPrice: 0
+})
+
 function handleUpdate(event: Event, field: Setting) {
-  const value = (event.target as HTMLInputElement).value
+  let value: string | number = (event.target as HTMLInputElement).value
 
   if (!value) {
     return
   }
 
+  value = Number(value)
+
+  defaultSetting[field] = value
+
   updateSetting({
-    [field]: Number(value)
+    [field]: value
   })
 }
+
+watchEffect(() => {
+  if (settings.value) {
+    for (const key in settings.value as SettingType) {
+      const field = key as Setting
+      defaultSetting[field] = settings.value[field]!
+    }
+  }
+})
 </script>
 
 <template>
@@ -31,7 +54,7 @@ function handleUpdate(event: Event, field: Setting) {
         id="min-nights"
         type="number"
         :disabled="isUpdating"
-        :model-value="settings?.minBookingLength"
+        :model-value="defaultSetting.minBookingLength"
         @blur="handleUpdate($event, 'minBookingLength')"
       />
     </FormRow>
@@ -40,7 +63,7 @@ function handleUpdate(event: Event, field: Setting) {
         id="max-nights"
         type="number"
         :disabled="isUpdating"
-        :model-value="settings?.maxBookingLength"
+        :model-value="defaultSetting.maxBookingLength"
         @blur="handleUpdate($event, 'maxBookingLength')"
       />
     </FormRow>
@@ -49,7 +72,7 @@ function handleUpdate(event: Event, field: Setting) {
         id="max-guests"
         type="number"
         :disabled="isUpdating"
-        :model-value="settings?.maxGuestsPerBooking"
+        :model-value="defaultSetting.maxGuestsPerBooking"
         @blur="handleUpdate($event, 'maxGuestsPerBooking')"
       />
     </FormRow>
@@ -58,7 +81,7 @@ function handleUpdate(event: Event, field: Setting) {
         id="breakfast-price"
         type="number"
         :disabled="isUpdating"
-        :model-value="settings?.breakfastPrice"
+        :model-value="defaultSetting.breakfastPrice"
         @blur="handleUpdate($event, 'breakfastPrice')"
       />
     </FormRow>
